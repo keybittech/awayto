d2722c684997093c23f1a5ec68010d8481c8d8ca
joe mccormick <joe.c.mccormick@gmail.com>
2021-10-17 13:46:17 -0700

    Implement create account cli

---
+++ bin/createAccount.mjs
@@ -0,0 +1,47 @@
+import { CognitoIdentityProviderClient, SignUpCommand, AdminConfirmSignUpCommand } from '@aws-sdk/client-cognito-identity-provider';
+import { ask } from './tool.mjs'
+
+
+const cipClient = new CognitoIdentityProviderClient();
+
+export default async function(props = {}) {
+
+  if (!Object.keys(props).length) {
+    props = {
+      clientId: await ask('Client Id:\n> '),
+      poolId: await ask('Pool Id:\n> '),
+      username: await ask('Username:\n> '),
+      password: await ask('Password:\n> '),
+      email: await ask('Email:\n> ')
+    }
+  }
+
+  try {
+
+    await cipClient.send(new SignUpCommand({
+      ClientId: props.clientId,
+      Username: props.username,
+      Password: props.password,
+      UserAttributes: [
+        {
+          Name: 'email',
+          Value: props.email
+        },
+        {
+          Name: 'custom:admin',
+          Value: 'public:guest'
+        }
+      ]
+    }));
+    
+    await cipClient.send(new AdminConfirmSignUpCommand({
+      UserPoolId: props.poolId,
+      Username: props.username
+    }));
+
+  } catch (error) {
+    console.log(error);
+  }
+
+  process.exit();
+}
--- bin/index.mjs
+++ bin/index.mjs
@@ -3,9 +3,14 @@
 import Test from './test.mjs';
 import Install from './install.mjs';
 import Uninstall from './uninstall.mjs';
+import CreateAccount from './createAccount.mjs';
 import { ask } from './tool.mjs';
 
-const action = await ask('Pick a task (0):\n0. install - this will install Awayto into the current directory (~10 minutes)\n1. uninstall - remove all Awayto resources locally and deployed by id (~1 min)\n> ') || '0';
+const action = await ask(`Pick a task (0):
+0. install - this will install Awayto into the current directory (~10 minutes)
+1. uninstall - remove all Awayto resources locally and deployed by id (~1 min)
+2. create account - create an admin account for a deployment
+> `) || '0';
 
 switch (action) {
   case 'test':
@@ -16,6 +21,9 @@ switch (action) {
     break;
   case '1':
     Uninstall();
+    break;
+  case '2':
+    CreateAccount();
     break;
   default:
     break;
